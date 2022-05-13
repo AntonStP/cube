41c9633bf04a0a48e0104e6e8786956dcb871653
aUngurian <a.ungurian@peppers-studio.ru>
2022-05-13 11:19:56 +0300

    chore(cube): исправлено катание при повторном нажатии

--- app/components/project/cube/cube.js
+++ app/components/project/cube/cube.js
@@ -4,32 +4,32 @@ document.ondragstart = ()=> {return false;};
 
 
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) { //для touch устройств
-    document.addEventListener("touchstart", function (event) {
-        const target = event.target.closest(".cube__active-zone");
-        if (!target) return;
-        const coordStart=event.touches[0];
-        let touchStart = {x:coordStart.pageX, y:coordStart.pageY};
-        cubeZone.ontouchmove = function (event) {
-            let touchMove = {x:event.touches[0].pageX, y:event.touches[0].pageY};
-            let rotY = ((touchMove.x - touchStart.x)/360).toFixed(2);
-            let rotX = ((-touchMove.y + touchStart.y)/360).toFixed(2);
-            cube.style.transform = `rotateY(${rotY}turn) rotateX(${rotX}turn)`;
-        };
-        document.addEventListener('touchend', function () {
-            cubeZone.touchmove=null;
-            touchStart = {};
-        });
-    });
-
-
+    // document.addEventListener("touchstart", function (event) {
+    //     const target = event.target.closest(".cube__active-zone");
+    //     if (!target) return;
+    //     const coordStart=event.touches[0];
+    //     let touchStart = {x:coordStart.pageX, y:coordStart.pageY};
+    //     let touchMove;
+    //     cubeZone.ontouchmove = function (event) {
+    //         touchMove = {x:event.touches[0].pageX, y:event.touches[0].pageY};
+    //         let rotY = ((touchMove.x - touchStart.x)/360).toFixed(2);
+    //         let rotX = ((-touchMove.y + touchStart.y)/360).toFixed(2);
+    //         cube.style.transform = `rotateY(${rotY}turn) rotateX(${rotX}turn)`;
+    //     };
+    //     document.addEventListener('touchend', function () {
+    //         cubeZone.touchmove=null;
+    //         touchStart = touchMove;
+    //     });
+    // });
 
 } else { //для компов
     document.addEventListener("pointerdown", function (event) {
         const target = event.target.closest(".cube__active-zone");
         if (!target) return;
         let touchStart = {x:event.clientX, y:event.clientY};
+        let touchMove;
         cubeZone.onpointermove = function (event) {
-            let touchMove ={x:event.clientX, y:event.clientY};
+            touchMove ={x:event.clientX, y:event.clientY};
             let rotY = ((touchMove.x - touchStart.x)/360).toFixed(2);
             let rotX = ((-touchMove.y + touchStart.y)/360).toFixed(2);
             cube.style.transform = `rotateY(${rotY}turn) rotateX(${rotX}turn)`;
@@ -37,7 +37,7 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phon
         };
         document.addEventListener('pointerup', function () {
             cubeZone.onpointermove=null;
-            touchStart = {};
+            touchStart = touchMove;
         });
     });
 
