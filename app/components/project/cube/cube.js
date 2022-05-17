6c7887f6658415e8f9cbe8dd3ce766fbb86ac239
aUngurian <a.ungurian@peppers-studio.ru>
2022-05-17 11:50:28 +0300

    chore(cube): оптимизация, добавлены фразы

--- app/components/project/cube/cube.js
+++ app/components/project/cube/cube.js
@@ -1,7 +1,15 @@
 import {randomInteger} from "../utils/randomInteger";
+import {frazes} from "./frazes";
 
+let state = "start";//состояние для проверки, что вообще происходит
+//константы для контейнеров
 const cube = document.querySelector('.cube');
+const fraze = document.querySelector('.custom-page__fraze');
 const cubeZone = document.querySelector('.cube__active-zone');
+//константы координат для слушателей
+let touchStart;
+let touchMove;
+
 document.ondragstart = ()=> {return false;};
 
 
@@ -9,21 +17,23 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phon
     document.addEventListener("touchstart", function (event) {
         const target = event.target.closest(".cube__active-zone");
         if (!target) return;
+
+        (state==="start") ? null : fraze.innerHTML = "бесплатные спины, можешь ещё покрутить";
         cube.classList.remove('cube_rotating');
+
         const coordStart=event.touches[0];
-        let touchStart = {x:coordStart.pageX, y:coordStart.pageY};
-        let touchMove;
+        touchStart = {x:coordStart.pageX, y:coordStart.pageY};
         cubeZone.ontouchmove = function (event) {
             touchMove = {x:event.touches[0].pageX, y:event.touches[0].pageY};
-            let rotY = ((touchMove.x - touchStart.x)/100).toFixed(2);
-            let rotX = ((-touchMove.y + touchStart.y)/100).toFixed(2);
-            cube.style.transform = `rotateY(${rotY}turn) rotateX(${rotX}turn)`;
+            const choord = calcRotation(touchStart, touchMove);
+            cube.style.transform = `rotateY(${choord.rotY}turn) rotateX(${choord.rotX}turn)`;
+
         };
         document.addEventListener('touchend', function () {
-            cubeZone.touchmove=null;
+            cubeZone.ontouchmove=null;
             touchStart = touchMove;
-            consoleTransform(randomInteger(1,6));
-            document.onpointerup=null;
+            rotatingWork(randomInteger(1,6));
+            document.ontouchend=null;
         });
     });
 
@@ -31,30 +41,43 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phon
     document.addEventListener("pointerdown", function (event) {
         const target = event.target.closest(".cube__active-zone");
         if (!target) return;
+
+        (state==="start") ? null : fraze.innerHTML = "бесплатные спины, можешь ещё покрутить";
         cube.classList.remove('cube_rotating');
-        let touchStart = {x:event.clientX, y:event.clientY};
-        let touchMove;
+
+        touchStart = {x:event.clientX, y:event.clientY};
         cubeZone.onpointermove = function (event) {
             touchMove ={x:event.clientX, y:event.clientY};
-            let rotY = ((touchMove.x - touchStart.x)/100).toFixed(2);
-            let rotX = ((-touchMove.y + touchStart.y)/100).toFixed(2);
-            cube.style.transform = `rotateY(${rotY}turn) rotateX(${rotX}turn)`;
+            const choord = calcRotation(touchStart, touchMove);
+            cube.style.transform = `rotateY(${choord.rotY}turn) rotateX(${choord.rotX}turn)`;
         };
         document.onpointerup = function () {
             cubeZone.onpointermove=null;
             touchStart = touchMove;
-            consoleTransform(randomInteger(1,6));
+            rotatingWork(randomInteger(1,6));
             document.onpointerup=null;
+            console.log("pointer-up")
         };
     });
 
 }
 
-function consoleTransform(side) {
+
+
+
+
+function calcRotation(startChoord,moveChoord) {
+    const rotX = ((-moveChoord.y + startChoord.y)/150).toFixed(2);
+    const rotY = ((moveChoord.x - startChoord.x)/150).toFixed(2);
+    return {rotX:rotX, rotY:rotY}
+}
+
+function rotatingWork(side) {
     console.log("transform", cube.style.transform);
     console.log("side", side)
     setTimeout(()=> {
         cube.classList.add('cube_rotating');
+        fraze.innerHTML = frazes[randomInteger(0,frazes.length)];
         switch (side) {
             case 1 :
                 cube.style.transform = `rotateY(${-0.01}turn) rotateX(${-0.01}turn)`;
@@ -75,5 +98,6 @@ function consoleTransform(side) {
                 cube.style.transform = `rotateY(${-0.49}turn) rotateX(${-0.24}turn)`;
                 break;
         }
-    },2010)
+        state = "rotated";
+    },2010);
 }
